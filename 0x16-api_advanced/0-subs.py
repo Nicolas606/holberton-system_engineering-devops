1192de3c3a485d71ee4002599b286ea50f8d49b6
Nicolas606 <castillonicolas606@gmail.com>
2021-09-29 22:16:43 -0500

    Fix pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     ''' returns the number of subscribers '''
     URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     json_reddit = requests.get(URL, allow_redirects=False)
-    if  json_reddit.status_code == 200:
-        return  json_reddit.json()['data']['subscribers']
+    if json_reddit.status_code == 200:
+        return json_reddit.json()['data']['subscribers']
     else:
         return 0
